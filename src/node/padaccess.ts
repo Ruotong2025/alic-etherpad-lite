44fe225e90a5308d90dce4239fbc8f402da7f83a
Laura <18811321306@163.com>
2025-12-24 11:33:15 +0800

    修改导出文件无法导出的问题

--- src/node/padaccess.ts
+++ src/node/padaccess.ts
@@ -4,8 +4,10 @@ const securityManager = require('./db/SecurityManager');
 // checks for padAccess
 module.exports = async (req: { params?: any; cookies?: any; session?: any; }, res: { status: (arg0: number) => { (): any; new(): any; send: { (arg0: string): void; new(): any; }; }; }) => {
   const {session: {user} = {}} = req;
+  // Use a default userName when authentication is not required
+  const userName = req.cookies.token || 'anonymous';
   const accessObj = await securityManager.checkAccess(
-      req.params.pad, req.cookies.sessionID, req.cookies.token, user);
+      req.params.pad, req.cookies.sessionID, userName, user);
 
   if (accessObj.accessStatus === 'grant') {
     // there is access, continue
